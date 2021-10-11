28dfb709ee9aec7ffc12fb9421bf06c857b211c4
Sam-Miller <samuelmi@unc.edu>
2021-10-11 10:28:45 -0400

    Added code to execute ML

--- app/src/main/java/com/example/toyproblem/MainActivity.java
+++ app/src/main/java/com/example/toyproblem/MainActivity.java
@@ -3,7 +3,14 @@ package com.example.toyproblem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import net.sf.javaml.classification.KNearestNeighbors;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.DenseInstance;
+import net.sf.javaml.core.Instance;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View v) {
+        // Imagine index = 0 is the height in cm and index = 1 is the weight in lbs
+        Instance e1 = new DenseInstance(new double[]{170, 137}, "Bear");
+        Instance e2 = new DenseInstance(new double[]{165, 145}, "Bear");
+        Instance e3 = new DenseInstance(new double[]{120, 2000}, "Buffalo");
+        Instance e4 = new DenseInstance(new double[]{127, 2200}, "Buffalo");
+
+        Instance e5 = new DenseInstance(new double[]{135, 2500});
+
+        Dataset dset = new DefaultDataset(); // Creates dataset
+        dset.add(e1);
+        dset.add(e2);
+        dset.add(e3);
+        dset.add(e4);
+
+        // Train model (using k-nearest neighbor)
+        KNearestNeighbors knn = new KNearestNeighbors(3); // Because we have 4 examples you should normally pick 2 but want an odd number for tie breakers when voting
+        knn.buildClassifier(dset);
+
+        Log.v("BearBuffaloClassifier", "Classifier created");
+
+        Object result = knn.classify(e5); // classify method just returns an Object
+        Log.v("BearBuffaloClassifier", "Class: " + result.toString());
 
     }
 }
